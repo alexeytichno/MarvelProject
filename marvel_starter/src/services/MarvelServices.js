d00f79c8ba800e8cf1144e0f6bab776ba93f5261
Lesha <alexeytichno@mail.ru>
2022-06-17 12:20:11 +0300

    spinner

---
+++ marvel_starter/src/services/MarvelServices.js
@@ -0,0 +1,37 @@
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=dcfc5d2a9d1aeae38bc1cc1c7b458a5f';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch ${url}, status ${res.status}`)
+        }
+    
+        return await res.json();
+    }
+    
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+                name: char.name,
+                description: char.description,
+                thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+                homepage: char.urls[0].url,
+                wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
